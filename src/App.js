702ab053e04b31ddb71eaea0b501a591db45b30b
RaghRoog <szumon612@interia.pl>
2023-07-20 13:26:55 +0200

    installed react router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { useEffect } from "react";
+import Home from "./components/Home";
 
 function App() {
+
+  useEffect(()=>{
+    // fetch('https://dummyjson.com/products/categories')
+    // .then(res => res.json())
+    // // .then(res => res.products.forEach(element => {
+    // //   console.log(element.category)
+    // // }))
+    // .then(console.log)
+  }, [])
+
   return (
     <div>
-
+      <Home/>
     </div>
   );
 }
