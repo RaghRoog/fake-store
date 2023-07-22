a0b6f03ef77547e5d7e5b9f09e5901e160e153b7
RaghRoog <szumon612@interia.pl>
2023-07-22 22:05:43 +0200

    page heading functionality

--- src/components/CategoryPage.js
+++ src/components/CategoryPage.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export default function CategoryPage() {
 
@@ -6,11 +6,38 @@ export default function CategoryPage() {
         fetch(`https://dummyjson.com/products/category/${localStorage.getItem('category')}`)
         .then(res => res.json())
         .then(console.log);
+        pageHeading()
     }, [])
+
+    let [category, setCategory] = useState('')
+
+    function pageHeading() {
+        let category = localStorage.getItem('category')
+        switch(category){
+            case 'smartphones':
+                setCategory('Smartphones')
+                break
+            case 'laptops':
+                setCategory('Laptops')
+                break
+            case 'fragrances':
+                setCategory('Fragrances')
+                break
+            case 'skincare':
+                setCategory('Skincare')
+                break
+            case 'groceries':
+                setCategory('Groceries')
+                break
+            case 'home-decoration':
+                setCategory('Home decoration')
+                break
+        }
+    }
 
     return(
         <div className="category-page">
-            <h1 className="cat-name">{localStorage.getItem('category')}</h1>
+            <h1 className="cat-name">{category}</h1>
             <div className="filters-container"></div>
             <div className="products-container"></div>
         </div>
